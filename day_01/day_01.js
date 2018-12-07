6da465d06c075b3f7d437054c1fd6377d3e669a0
João Seixas <up201505648@fe.up.pt>
2018-12-07 10:05:51 +0000

    day 01 finished

--- day_01/day_01.js
+++ day_01/day_01.js
@@ -2,13 +2,52 @@ let fs = require('fs');
 let inputFile = fs.readFileSync('input.txt', 'utf8');
 let input = inputFile.split('\n');
 
-let exer01 = function() {
+let part_1 = function() {
     let a = 0;
     input.forEach(element => {
         a += parseInt(element);
-        console.log(a);
     });
+    console.log(a);
 }
 
 
-exer01();
+// Very costly and needs a lot of time (change to set)
+let part_2 = function() {
+    let twices = [];
+    let a = 0;
+    twices.push(a);
+
+    while(true) {
+        for (let i = 0; i < input.length; i++) {
+            a += parseInt(input[i]);
+            
+            if (twices.indexOf(a) > -1) {
+                console.log(a);
+                return;
+            } else 
+            twices.push(a);
+        }
+    }
+}
+
+// Way better solution 
+let part_2_set = function(){
+    let twices = new Set();
+    let a = 0;
+    twices.add(a);
+
+    while(true) {
+        for (let i = 0; i < input.length; i++) {
+            a += parseInt(input[i]);
+            
+            if (twices.has(a)) {
+                console.log(a);
+                return;
+            } else 
+            twices.add(a);
+        }
+    }
+}
+
+part_1();
+part_2_set();
